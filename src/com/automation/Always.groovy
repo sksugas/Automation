f1d9227f81f07a13c1dd72ba2443979cccfe337e
sksugas <sksugas@gmail.com>
2020-03-10 12:44:33 +0530

    Update Always.groovy

--- src/com/automation/Always.groovy
+++ src/com/automation/Always.groovy
@@ -1,1 +1,8 @@
 
+package com.automation;
+
+
+def alwaysMethod(def Src_Path,def TestType,config){
+        def emailnotification = new com.automation.emailNotification()
+        emailnotification.emailNotificationMethod("${Src_Path}","${TestType}",config)
+}
